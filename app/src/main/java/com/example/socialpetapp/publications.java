dc4b513b75ebba39d683e764340f5f69070dd15d
adrian <adriancrack123@gmail.com>
2022-05-09 20:09:37 +0200

    Barra navegacion2

---
+++ app/src/main/java/com/example/socialpetapp/publications.java
@@ -0,0 +1,14 @@
+package com.example.socialpetapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class publications extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_publications);
+    }
+}
